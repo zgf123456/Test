f0bf2f47a3db8359787aabd1dc02e6128f02a6db
zgf <guofengzheng1@creditease.cn>
2016-04-25 11:14:57 +0800

    dsf

---
+++ src/main/java/com/zgf/Test/java8/lambda/BasicTest.java
@@ -0,0 +1,10 @@
+package com.zgf.Test.java8.lambda;
+
+import com.google.common.base.Predicate;
+
+public class BasicTest {
+	public static void main(String[] args) {
+		Predicate<Integer> predicate = x -> x > 5;
+		
+	}
+}
