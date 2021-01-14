80cbef1021cb14b25bc2ca496899ca6ca2b6faac
郑 <46139753@qq.com>
2021-01-14 14:40:03 +0800

    a

--- src/main/java/com/zgf/Test/gusi/GuPiaoInfo.java
+++ src/main/java/com/zgf/Test/gusi/GuPiaoInfo.java
@@ -8,11 +8,12 @@ public class GuPiaoInfo {
 
     static {
         guPiaoInfos.add(new GuPiaoInfo("sh510300", "300ETF", "3"));
-        guPiaoInfos.add(new GuPiaoInfo("sh600036", "招商", "zs", true));
-        guPiaoInfos.add(new GuPiaoInfo("sh601939", "建设", "js"));
-        guPiaoInfos.add(new GuPiaoInfo("sz002007", "华兰", "hl"));
-        guPiaoInfos.add(new GuPiaoInfo("sz000338", "淮柴", "hc"));
-        guPiaoInfos.add(new GuPiaoInfo("sh600702", "舍得", "sd"));
+        guPiaoInfos.add(new GuPiaoInfo("sh600036", "招商", "ZS", true));
+        guPiaoInfos.add(new GuPiaoInfo("sh601939", "建设", "JS"));
+        guPiaoInfos.add(new GuPiaoInfo("sz002007", "华兰", "HL"));
+        guPiaoInfos.add(new GuPiaoInfo("sz000338", "淮柴", "HC"));
+        guPiaoInfos.add(new GuPiaoInfo("sh600702", "舍得", "SD"));
+        guPiaoInfos.add(new GuPiaoInfo("sh600030", "中信", "ZX"));
     }
 
     private String id;
