dfac35716036f6ccaccc7573cd6a5abcaa5fa642
郑 <46139753@qq.com>
2021-01-26 16:02:46 +0800

    回溯法解数独和k链表合并问题

--- src/main/java/com/zgf/Test/aligorthm/leetcode/MergeListNode.java
+++ src/main/java/com/zgf/Test/aligorthm/leetcode/MergeListNode.java
@@ -33,6 +33,13 @@ public class MergeListNode {
         }
     }
 
+
+    /**
+     * 多个一起合并
+     *
+     * @param lists
+     * @return
+     */
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) return null;
         ListNode head = null;
@@ -76,6 +83,52 @@ public class MergeListNode {
 
         return head;
     }
+
+//    /**
+//     * 归并算法的思路
+//     *
+//     * @param lists
+//     * @return
+//     */
+//    public ListNode mergeKLists2(ListNode[] lists) {
+//        if (lists == null || lists.length == 0) return null;
+//        if (lists.length == 1) return lists[0];
+//        doMergeKLists2(lists, 0, lists.length - 1);
+//    }
+//
+//    public ListNode doMergeKLists2(ListNode[] lists, int start, int end) {
+//        int nums = end - start + 1;
+//        if (nums == 1) {
+//            return lists[start];
+//        } else if (nums > 2) {
+//            int mid = (end - start) / 2;
+//            ListNode listNode1 = doMergeKLists2(lists, start, mid);
+//            ListNode listNode2 = doMergeKLists2(lists, mid + 1, end);
+//            return doMergeKLists2_do(listNode1, listNode2);
+//        } else {
+//            return doMergeKLists2_do(lists[start], lists[start + 1]);
+//        }
+//    }
+//
+//    private ListNode doMergeKLists2_do(ListNode listNode1, ListNode listNode2) {
+//        ListNode head = null;
+//        ListNode head1 = listNode1;
+//        ListNode head2 = listNode2;
+//
+//        if (head1.val < head2.val) {
+//            head = head1;
+//            head1 = head1.next;
+//        } else {
+//            head = head2;
+//            head2 = head2.next;
+//        }
+//
+//        while (true) {
+//            if (head1 == null && head2 == null) break;
+//        }
+//
+//        return head;
+//    }
 }
 
 class ListNode {
