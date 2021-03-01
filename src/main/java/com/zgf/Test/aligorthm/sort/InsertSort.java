5bdc990bcc77ced590afe24834254ab6abbdbaa1
郑 <46139753@qq.com>
2021-03-01 17:48:16 +0800

    排序

--- src/main/java/com/zgf/Test/aligorthm/sort/InsertSort.java
+++ src/main/java/com/zgf/Test/aligorthm/sort/InsertSort.java
@@ -24,15 +24,12 @@ public class InsertSort {
     private void sort(int[] arr) {
         //插入排序
         for (int i = 1; i < arr.length; i++) {
-            //外层循环，从第二个开始比较
             for (int j = i; j > 0; j--) {
-                //内存循环，与前面排好序的数据比较，如果后面的数据小于前面的则交换
                 if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                 } else {
-                    //如果不小于，说明插入完毕，退出内层循环
                     break;
                 }
             }
