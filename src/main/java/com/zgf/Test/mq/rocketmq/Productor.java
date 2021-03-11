558ce9a00b8039a89469d0e94491b347464f4b38
郑 <46139753@qq.com>
2021-03-11 13:35:45 +0800

    rocketmq

---
+++ src/main/java/com/zgf/Test/mq/rocketmq/Productor.java
@@ -0,0 +1,31 @@
+package com.zgf.Test.mq.rocketmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class Productor {
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("zgf");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("10.143.150.183:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 10; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("ZgfTopicTest" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " +
+                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
