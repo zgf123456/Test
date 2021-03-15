8309558188fbe80a7657f542543ac1f49004a722
郑 <46139753@qq.com>
2021-03-15 11:23:50 +0800

    rocketmq

--- src/main/java/com/zgf/Test/mq/rocketmq/productor/TransactionProducer.java
+++ src/main/java/com/zgf/Test/mq/rocketmq/productor/TransactionProducer.java
@@ -17,6 +17,13 @@ import java.util.concurrent.atomic.AtomicInteger;
  * 事务消息
  * <p>
  * 事务消息不支持延时消息和批量消息。
+ *
+ *
+ * 事务消息共有三种状态，提交状态、回滚状态、中间状态：
+
+ TransactionStatus.CommitTransaction: 提交事务，它允许消费者消费此消息。
+ TransactionStatus.RollbackTransaction: 回滚事务，它代表该消息将被删除，不允许被消费。
+ TransactionStatus.Unknown: 中间状态，它代表需要检查消息队列来确定状态。
  */
 public class TransactionProducer {
     public static void main(String[] args) throws Exception {
