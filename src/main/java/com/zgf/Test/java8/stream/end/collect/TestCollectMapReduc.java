0e886720551057ec0da40a695e1037b7c494179f
郑 <46139753@qq.com>
2021-04-01 18:46:36 +0800

    java8-stream

--- src/main/java/com/zgf/Test/java8/stream/end/collect/TestCollectMapReduc.java
+++ src/main/java/com/zgf/Test/java8/stream/end/collect/TestCollectMapReduc.java
@@ -1,13 +1,25 @@
 package com.zgf.Test.java8.stream.end.collect;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
- * 字符串连接器
+ * mapping reducing
  *
  * @author zgf
  * @date 2021-04-01 下午3:16
  */
 public class TestCollectMapReduc {
     public static void main(String[] args) {
+        Map<Boolean, List<Integer>> collect = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+                .collect(Collectors.mapping(x -> x * x, Collectors.partitioningBy(x -> x > 10)));
+        System.out.println(collect);
 
+        Optional<Integer> collect1 = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+                .collect(Collectors.reducing((x, y) -> x + y));
+        System.out.println(collect1.get());
     }
 }
